6e192114d81eeb1c960078c74a3c7cba318a65c8
lucia <analucia0189@gmail.com>
2015-10-14 19:40:45 -0600

    lat and lng

--- app/src/main/java/com/example/lucia/mymaps/MainActivity.java
+++ app/src/main/java/com/example/lucia/mymaps/MainActivity.java
@@ -5,25 +5,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity {
 
     GoogleMap mMap;
     private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final double SEATTLE_LAT=47.606, SEATTLE_LNG = -122.33, SYDNEY_LAT = -33.86,SYDNEY_LNG = 151.20,
+                                NEWYORK_LAT = 40.714, NEWYORK_LNG = -74.0056;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(servicesOK()){
+        if (servicesOK()) {
             setContentView(R.layout.activity_map);
-            Toast.makeText(this, "Ready to map", Toast.LENGTH_LONG);
-        }else{
+            if (initMap()) {
+                Toast.makeText(this, "Ready to map", Toast.LENGTH_LONG);
+                goToLocation(SEATTLE_LAT,SEATTLE_LNG,5);
+            } else {
+                Toast.makeText(this, "Map not connected", Toast.LENGTH_LONG);
+            }
+        } else {
             setContentView(R.layout.activity_main);
         }
     }
@@ -50,16 +63,33 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public boolean servicesOK(){
+    public boolean servicesOK() {
         int isAvailabe = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if(isAvailabe == ConnectionResult.SUCCESS){
+        if (isAvailabe == ConnectionResult.SUCCESS) {
             return true;
-        }else if(GooglePlayServicesUtil.isUserRecoverableError(isAvailabe)){
-            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailabe,this,ERROR_DIALOG_REQUEST);
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailabe)) {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(isAvailabe, this, ERROR_DIALOG_REQUEST);
             dialog.show();
-        }else {
+        } else {
             Toast.makeText(this, "Cannot connect to mapping service", Toast.LENGTH_LONG);
         }
         return false;
+    }
+
+    private boolean initMap() {
+        if (mMap == null) {
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            mMap = mapFragment.getMap();
+        }
+        return (mMap != null);
+    }
+
+        private void goToLocation(double lat, double lng, float zoom){
+            LatLng latLng = new LatLng(lat, lng);
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng,zoom);
+            mMap.moveCamera(update);
+        }
+
+    public void geoLocate(View view) {
     }
 }
